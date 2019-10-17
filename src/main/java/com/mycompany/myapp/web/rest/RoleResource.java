901f7afe33a590069aeb63215114f9acb5a1c512
huangzhongjie <1602299512@qq.com>
2019-10-17 16:50:42 +0800

    初始化代码

---
+++ src/main/java/com/mycompany/myapp/web/rest/RoleResource.java
@@ -0,0 +1,129 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.service.RoleService;
+import com.mycompany.myapp.web.rest.errors.BadRequestAlertException;
+import com.mycompany.myapp.service.dto.RoleDTO;
+
+import io.github.jhipster.web.util.HeaderUtil;
+import io.github.jhipster.web.util.PaginationUtil;
+import io.github.jhipster.web.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * REST controller for managing {@link com.mycompany.myapp.domain.Role}.
+ */
+@RestController
+@RequestMapping("/api")
+public class RoleResource {
+
+    private final Logger log = LoggerFactory.getLogger(RoleResource.class);
+
+    private static final String ENTITY_NAME = "microservice1Role";
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+    private final RoleService roleService;
+
+    public RoleResource(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    /**
+     * {@code POST  /roles} : Create a new role.
+     *
+     * @param roleDTO the roleDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new roleDTO, or with status {@code 400 (Bad Request)} if the role has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("/roles")
+    public ResponseEntity<RoleDTO> createRole(@RequestBody RoleDTO roleDTO) throws URISyntaxException {
+        log.debug("REST request to save Role : {}", roleDTO);
+        if (roleDTO.getId() != null) {
+            throw new BadRequestAlertException("A new role cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        RoleDTO result = roleService.save(roleDTO);
+        return ResponseEntity.created(new URI("/api/roles/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PUT  /roles} : Updates an existing role.
+     *
+     * @param roleDTO the roleDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated roleDTO,
+     * or with status {@code 400 (Bad Request)} if the roleDTO is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the roleDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PutMapping("/roles")
+    public ResponseEntity<RoleDTO> updateRole(@RequestBody RoleDTO roleDTO) throws URISyntaxException {
+        log.debug("REST request to update Role : {}", roleDTO);
+        if (roleDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        RoleDTO result = roleService.save(roleDTO);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, roleDTO.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code GET  /roles} : get all the roles.
+     *
+     * @param pageable the pagination information.
+     * @param queryParams a {@link MultiValueMap} query parameters.
+     * @param uriBuilder a {@link UriComponentsBuilder} URI builder.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of roles in body.
+     */
+    @GetMapping("/roles")
+    public ResponseEntity<List<RoleDTO>> getAllRoles(Pageable pageable, @RequestParam MultiValueMap<String, String> queryParams, UriComponentsBuilder uriBuilder) {
+        log.debug("REST request to get a page of Roles");
+        Page<RoleDTO> page = roleService.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(uriBuilder.queryParams(queryParams), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    /**
+     * {@code GET  /roles/:id} : get the "id" role.
+     *
+     * @param id the id of the roleDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the roleDTO, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/roles/{id}")
+    public ResponseEntity<RoleDTO> getRole(@PathVariable Long id) {
+        log.debug("REST request to get Role : {}", id);
+        Optional<RoleDTO> roleDTO = roleService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(roleDTO);
+    }
+
+    /**
+     * {@code DELETE  /roles/:id} : delete the "id" role.
+     *
+     * @param id the id of the roleDTO to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/roles/{id}")
+    public ResponseEntity<Void> deleteRole(@PathVariable Long id) {
+        log.debug("REST request to delete Role : {}", id);
+        roleService.delete(id);
+        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
+    }
+}
